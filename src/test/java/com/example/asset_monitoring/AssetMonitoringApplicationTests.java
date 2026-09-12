da777b16ea67656cc0a15bffc5d4c6fc53d0a1c7
alisha971 <119912853+alisha971@users.noreply.github.com>
2026-09-12 19:46:13 +0530

    Initial Commit

---
+++ src/test/java/com/example/asset_monitoring/AssetMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asset_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

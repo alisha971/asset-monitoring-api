303d85edb2955bee3c783329202fc6bd9fafc083
alisha971 <119912853+alisha971@users.noreply.github.com>
2026-09-12 20:35:51 +0530

    Ping Test

--- src/main/java/com/example/asset_monitoring/AssetMonitoringApplication.java
+++ src/main/java/com/example/asset_monitoring/AssetMonitoringApplication.java
@@ -3,11 +3,24 @@ package com.example.asset_monitoring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication // starts the spring boot application
 public class AssetMonitoringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AssetMonitoringApplication.class, args);
 	}
 
+}
+
+@RestController //tells that this class contains http endpoints
+class HealthController{
+	
+	@GetMapping("/ping") //maps get req to "/ping" to this function
+	public String ping(){
+		return "asset-monitoring up";
+	}
 }

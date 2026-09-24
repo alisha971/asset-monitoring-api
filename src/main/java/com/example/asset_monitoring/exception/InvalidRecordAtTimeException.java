65a9fcdf65f2f9c656cc270ab41fbf1f5559bcb2
alisha971 <119912853+alisha971@users.noreply.github.com>
2026-09-24 22:02:57 +0530

    refactor: rename IllegalRecordAtTimeException to InvalidRecordAtTimeException

--- src/main/java/com/example/asset_monitoring/exception/InvalidRecordAtTimeException.java
+++ src/main/java/com/example/asset_monitoring/exception/InvalidRecordAtTimeException.java
@@ -2,8 +2,8 @@ package com.example.asset_monitoring.exception;
 
 import java.time.Instant;
 
-public class IllegalRecordAtTimeException extends RuntimeException{
-    public IllegalRecordAtTimeException(Instant recordedAt){
+public class InvalidRecordAtTimeException extends RuntimeException{
+    public InvalidRecordAtTimeException(Instant recordedAt){
         super("The measurement recorded at time " + recordedAt + " is in future and cannot be accepted");
     }
 }
